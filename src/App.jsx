b65119ce741292e4d6bfd45ba381902530fe2499
Vinicius Almeida <92161570+Vinicius-Almeidaa@users.noreply.github.com>
2024-06-13 13:24:15 +0000

    Exibindo itens carregados via API

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,76 @@
+import { useState } from 'react';
 import './App.css'
 import Card from './components/Card/Card'
+import { useEffect } from 'react';
 
 function App() {
-  const item1 = {
-    name: 'Vinicius',
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+
+  // const item1 = {
+
+  //   name: 'Rick Sanchez',
+
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+
+  // }
+
+  // const item2 = {
+
+  //   name: 'Morty Smith',
+
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+
+  // }
+
+  // const item3 = {
+
+  //   name: 'Summer Smith',
+
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+
+  // }
+
+  // const itens = [item1, item2, item3];
+
+  const [itens, setItens] = useState([]);
+
+ 
+
+  async function carregarDados() {
+
+    const apiUrl = 'https://rickandmortyapi.com/api/character';
+
+    const response = await fetch(apiUrl);
+
+    const body = await response.json();
+
+    const results = body.results;
+
+    console.log(results);
+
+    setItens(results);
+
   }
 
-  const item2 = {
-    name: "Edson",
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
-
-  const item3 = {
-    name: 'Kina',
-    image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
-  }
-
-  //Listas (Arrays)
-  const itens = [item1, item2, item3]
+ 
+  useEffect(function() {
+    carregarDados()
+  }, [])
+  
 
   return (
+
     <>
+
       <div className='cards'>
-        {/* para cada um dos itens da lista, exibir um Card*/}
-        {itens.map(elemento => <Card item={elemento} key={elemento.name} />)}
+
+        {itens.map((elemento) => <Card item={elemento} key={elemento.name} />)}
+
       </div>
+
     </>
-    
+
   )
+
 }
 
 export default App
